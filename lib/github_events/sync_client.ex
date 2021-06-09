3e74ccf0a03d98f7da831b7fbc3e0c46b8c4e963
TraceyOnim <traceyonim22@gmail.com>
2021-06-10 02:47:40 +0300

    created client to handle sync of github PR events

---
+++ lib/github_events/sync_client.ex
@@ -0,0 +1,25 @@
+defmodule GithubEvents.SyncClient do
+  use GenServer
+  alias Phoenix.PubSub
+
+  def start_link(repo, options \\ []) do
+    GenServer.start_link(__MODULE__, repo, options)
+  end
+
+  @impl true
+  def init(repo) do
+    {:ok, repo, {:continue, :sync}}
+  end
+
+  @impl true
+  def handle_continue(:sync, state) do
+    response = HTTPoison.get!("https://api.github.com/repos/#{state}/pulls")
+    {:noreply, response, {:continue, :publish}}
+  end
+
+  @impl true
+  def handle_continue(:publish, response) do
+    PubSub.broadcast(GithubEvents.PubSub, "PR_events", response.body)
+    {:stop, :normal, []}
+  end
+end
