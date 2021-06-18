0daff7c892d584fc7c13b6f00ca6be285dafa926
TraceyOnim <traceyonim22@gmail.com>
2021-06-19 01:24:47 +0300

    remap github user to another github user in the repo

--- lib/github_events_web/live/event_live.ex
+++ lib/github_events_web/live/event_live.ex
@@ -3,18 +3,22 @@ defmodule GithubEventsWeb.EventLive do
 
   alias GithubEvents.Account
   alias Phoenix.PubSub
+  alias GithubEventsWeb.Util
 
   @impl true
   def mount(_params, _session, socket) do
     PubSub.subscribe(GithubEvents.PubSub, "PR_events")
-    {:ok, socket |> assign(payloads: [], repos: [])}
+    {:ok, socket |> assign(payloads: [], repos: [], contributors: [])}
   end
 
   @impl true
   def render(assigns) do
     ~L"""
       <div class="container">
-      <form phx-change="sync">
+      <div class="row">
+      <div class="column column-30">
+      <h3>Github Pull Request Events </h3>
+       <form phx-change="sync">
       <select name="repo">
         <option value="all" selected>All</option>
         <%= for repo <- @repos do %>
@@ -22,7 +26,6 @@ defmodule GithubEventsWeb.EventLive do
         <% end %>
       </select>
       </form>
-      <h1>Github Pull Request Events </h1>
       <table>
       <tr>
       <th>url</th>
@@ -49,6 +52,19 @@ defmodule GithubEventsWeb.EventLive do
       
       </table>
       </div>
+      <div class="column column-offset-15">
+      <h4>Repo contributors</h4>
+      <table>
+    <%= for contributor <- @contributors do %>
+    <tr>
+    <td><%= contributor %></td>
+    <td><%= live_patch "Events", to: Routes.live_path(@socket, GithubEventsWeb.EventLive, owner: "#{contributor}") %></td>
+     </tr>
+    <% end %>
+    <table>
+      </div>
+      </div>
+      </div>
     """
   end
 
@@ -65,11 +81,42 @@ defmodule GithubEventsWeb.EventLive do
   @impl true
   def handle_event("sync", %{"repo" => repo}, socket) do
     GithubEvents.SyncClient.start_link(repo)
-    {:noreply, socket}
+
+    {:noreply, socket |> repo_contributors(repo) |> _remapped_user_repos(repo)}
   end
 
   @impl true
   def handle_info({:pull_request_events, payloads}, socket) do
     {:noreply, socket |> assign(payloads: Jason.decode!(payloads))}
+  end
+
+  defp repo_contributors(socket, repo) do
+    contributors =
+      Enum.map(repo_contributors(repo), fn contributor ->
+        profile = Util.github_user_profile!(contributor)
+        profile["name"]
+      end)
+
+    assign(socket, contributors: contributors)
+  end
+
+  defp repo_contributors(repo) do
+    case HTTPoison.get("https://api.github.com/repos/#{repo}/contributors") do
+      {:ok, response} -> response.body |> Jason.decode!() |> Enum.map(& &1["login"])
+      {:error, _} -> []
+    end
+  end
+
+  defp _remapped_user_repos(socket, repo) do
+    repos =
+      Enum.map(repo_contributors(repo), fn contributor ->
+        profile = Util.github_user_profile!(contributor)
+
+        contributor
+        |> Util.fetch_repos!()
+        |> Util.parse_repos()
+      end)
+
+    assign(socket, repos: repos)
   end
 end
