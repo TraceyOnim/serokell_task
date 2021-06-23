3391f7c816a53068ee4e1e8bc72761db195b6051
TraceyOnim <traceyonim22@gmail.com>
2021-06-24 01:45:53 +0300

    refactor

--- lib/github_events_web/live/util.ex
+++ lib/github_events_web/live/util.ex
@@ -1,4 +1,12 @@
 defmodule GithubEventsWeb.Util do
+  @moduledoc """
+  This Util module contains functions that can be invoked in more than one module in live folder
+  """
+
+  @doc """
+  fetches github user profile
+  """
+  @spec github_user_profile!(String.t()) :: map() | nil
   def github_user_profile!(github_user) do
     case HTTPoison.get!("https://api.github.com/users/#{github_user}") do
       %{status_code: 200} = profile -> Jason.decode!(profile.body)
@@ -6,6 +14,10 @@ defmodule GithubEventsWeb.Util do
     end
   end
 
+  @doc """
+  Fetches github user repos
+  """
+  @spec fetch_repos!(String.t()) :: [String.t(), ...] | nil
   def fetch_repos!(owner) do
     case HTTPoison.get!("https://api.github.com/users/#{owner}/repos") do
       %{status_code: 200} = repos -> repos
